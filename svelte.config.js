849bab6ccb2c238e6161e00089356e6afb60b2f8
benjababe <bengohzy@gmail.com>
2025-05-29 14:26:57 +0800

    feat: include azure static apps swa

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,11 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import azure from 'svelte-adapter-azure-swa';
 
 const config = {
   preprocess: vitePreprocess(),
-}
+  kit: {
+    adapter: azure()
+  }
+};
 
 export default config;
